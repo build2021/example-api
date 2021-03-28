bb495b8fe13a05a73afe5e11f449668a81dd14ec
build2021 <jenkins.machi.pon@gmail.com>
2021-03-28 19:31:33 +0900

    Update setvice setting

--- Example.Api/Program.cs
+++ Example.Api/Program.cs
@@ -12,6 +12,8 @@ namespace Example.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .UseSystemd()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
